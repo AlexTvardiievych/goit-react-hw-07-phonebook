68a4b8897f6cf214c6015442ea663ce3b6b2caab
AlexTvardGit <alex.tvard.git@gmail.com>
2021-10-30 15:12:23 +0300

    fix
    fix contacts display

--- src/components/ContactList/Contacts.styled.jsx
+++ src/components/ContactList/Contacts.styled.jsx
@@ -3,6 +3,7 @@ import styled from "@emotion/styled";
 export const List = styled.ul`
   list-style: none;
   display: flex;
+  flex-direction: column;
 `;
 export const Item = styled.li`
   padding: 5px;
